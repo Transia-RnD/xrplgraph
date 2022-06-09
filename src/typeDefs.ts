b9a1bf00f169c935c41a1e5338ac3a6ea0a79ddc
Denis Angell <denis@harpangell.com>
2022-06-09 00:22:38 -0400

    remove unused types

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,131 +1,31 @@
 import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
-  type NFTokenData {
-    NFTokenID: String!
-    URI: String!
-  }
-  type NFToken {
-    NFToken: NFTokenData!
-  }
-  type FinalFields {
-    Account: String!
-    Balance: Int!
-    Flags: Int!
-    MintedNFTokens: Int!
-    OwnerCount: Int!
-    Sequence: Int!
-    NFTokens: [NFToken]!
-  }
-  type PreviousFields {
-    Balance: String!
-    MintedNFTokens: Int!
-    Sequence: Int!
-    NFTokens: [NFToken]!
-  }
-  type ModifiedNode {
-    FinalFields: FinalFields!
-    LedgerEntryType: String!
-    LedgerIndex: String!
-    PreviousFields: PreviousFields!
-    PreviousTxnID: String!
-    PreviousTxnLgrSeq: Int!
-  }
-  type AffectedNode {
-    ModifiedNode: ModifiedNode!
-  }
-  type Meta {
-    AffectedNodes: [AffectedNode]!
-    TransactionIndex: Int!
-    TransactionResult: String!
-  }
-  type XrplResult {
-    Account: String!
-    Fee: String!
-    Flags: Int!
-    LastLedgerSequence: Int!
-    NFTokenTaxon: Int!
-    Sequence: Int!
-    SigningPubKey: String!
-    TransactionType: String!
-    TransferFee: Int!
-    TxnSignature: String!
-    URI: String!
-    date: Int!
-    hash: String!
-    inLedger: Int!
-    ledger_index: Int!
-    meta: Meta
-    validated: Boolean!
-  }
   type Collection {
     name: String!
     issuer: String!
     taxon: Int!
     family: String!
   }
-  type Domain {
-    source: String!
-    merchant: String!
-    network: String!
-    accountId: String!
-    userId: String!
-    type: String!
-    id: String!
-    version: String!
-  }
-  type MRVRecord {
+  type Attribute {
     name: String!
-    type: String!
+    description: String
+    value: String | Int
   }
-  type Types {
-    MRVRecord: [MRVRecord]!
-  }
-  type Message {
-    assetCId: String!
-    recordId: String!
-    tons: Int!
-    issuer: String!
-    issuerTag: Int!
-    recordedType: String!
-    recordedTime: Int!
-  }
-  type Data {
-    domain: Domain!
-    types: Types!
-    message: Message!
-  }
-  type IPFSv1Schema {
+  type XLS20Schema {
     schema: String!
     nftType: String!
     name: String!
     description: String!
     image: String!
     collection: Collection!
-    attributes: String!
-    address: String!
-    sig: String!
-    data: Data!
-  }
-  type TxNFToken {
-    Flags: Int!
-    Account: String!
-    NFTokenID: String!
-    NFTokenTaxon: Int!
-    URI: String
+    attributes: [Attribute]!
   }
   type NFTokenOffer {
     amount: Int!
     flags: Int!
     nft_offer_index: String!
     owner: String!
-  }
-  type QLNFToken {
-    id: String!
-    uri: String!
-    nft: TxNFToken
-    ipfs: IPFSv1Schema
-    offers: [NFTokenOffer]
   }
   type AccountNFToken {
     Flags: Int!
@@ -147,9 +47,7 @@ export const typeDefs = gql`
     scope: CacheControlScope
     inheritMaxAge: Boolean
   ) on FIELD_DEFINITION | OBJECT | INTERFACE | UNION
-
   type Query {
-    nftoken(id: String!): QLNFToken
     account_nfts(account: String!): [AccountNFToken]
   }
 `;
--- src/types/ipfs.ts
+++ src/types/ipfs.ts
@@ -1,36 +1,9 @@
-// {
-//   "schema": "URI://UriToXRArt.v0Schema",
-//   "nftType": "crc.v1",
-//   "name": "Carbonland Trust QmXaL MRVR II",
-//   "description": "Carbonland Trust Issuance II",
-//   "image": "ipfs://QmQYZve64WtZxW4yRzhfvDdC3hKxzELivgGqRwSemndvR7",
-//   "collection": {
-//     "name": "Asset QmXaL MRV Record Collection",
-//     "issuer": "rMmAbcTBnZXFTwEXG2GiGrcQWDj9p7s25C",
-//     "taxon": 0,
-//     "family": "carbonland"
-//   },
-//   "attributes": [],
-// }
-
 export interface Collection {
     name: string;
     issuer: string;
     taxon: number;
     family: string;
 }
-
-export interface Domain {
-    source: string;
-    merchant: string;
-    network: string;
-    accountId: string;
-    userId: string;
-    type: string;
-    id: string;
-    version: string;
-}
-
 export interface Attribute {
     name: string;
     description?: string;
